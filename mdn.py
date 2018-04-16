c53f95abba48498f804ecc0cfe62dfb1653097b3
Benjamin Bastian <benjamin.bastian@gmail.com>
2018-04-15 20:12:45 -0700

    Add MDN code

---
+++ mdn.py
@@ -0,0 +1,98 @@
+"""A module for a mixture density network layer
+
+For more info on MDNs, see _Mixture Desity Networks_ by Bishop, 1994.
+"""
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.distributions import Categorical
+import math
+
+
+ONEOVERSQRT2PI = 1.0 / math.sqrt(2*math.pi)
+
+class MDN(nn.Module):
+    """A mixture density network layer
+
+    The input maps to the parameters of a MoG probability distribution, where
+    each Gaussian has O dimensions and diagonal covariance.
+
+    Arguments:
+        in_features (int): the number of dimensions in the input
+        out_features (int): the number of dimensions in the output
+        num_gaussians (int): the number of Gaussians per output dimensions
+
+    Input:
+        minibatch (BxD): B is the batch size and D is the number of input
+            dimensions.
+
+    Output:
+        (pi, sigma, mu) (BxG, BxGxO, BxGxO): B is the batch size, G is the
+            number of Gaussians, and O is the number of dimensions for each
+            Gaussian. Pi is a multinomial distribution of the Gaussians. Sigma
+            is the standard deviation of each Gaussian. Mu is the mean of each
+            Gaussian.
+    """
+    def __init__(self, in_features, out_features, num_gaussians):
+        super(MDN, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.num_gaussians = num_gaussians
+        self.pi = nn.Sequential(
+            nn.Linear(in_features, num_gaussians),
+            nn.Softmax(dim=1)
+        )
+        self.sigma = nn.Linear(in_features, out_features*num_gaussians)
+        self.mu = nn.Linear(in_features, out_features*num_gaussians)
+
+    def forward(self, minibatch):
+        pi = self.pi(minibatch)
+        sigma = torch.exp(self.sigma(minibatch))
+        sigma = sigma.view(-1, self.num_gaussians, self.out_features)
+        mu = self.mu(minibatch)
+        mu = mu.view(-1, self.num_gaussians, self.out_features)
+        return pi, sigma, mu
+
+
+def gaussian_probability(sigma, mu, data):
+    """Returns the probability of `data` given MoG parameters `sigma` and `mu`.
+    
+    Arguments:
+        sigma (BxGxO): The standard deviation of the Gaussians. B is the batch
+            size, G is the number of Gaussians, and O is the number of
+            dimensions per Gaussian.
+        mu (BxGxO): The means of the Gaussians. B is the batch size, G is the
+            number of Gaussians, and O is the number of dimensions per Gaussian.
+        data (BxI): A batch of data. B is the batch size and I is the number of
+            input dimensions.
+
+    Returns:
+        probabilities (BxG): The probability of each point in the probability
+            of the distribution in the corresponding sigma/mu index.
+    """
+    data = data.unsqueeze(1).expand_as(sigma)
+    ret = ONEOVERSQRT2PI * torch.exp(-0.5 * ((data - mu) / sigma)**2) / sigma
+    return torch.prod(ret, 2)
+
+
+def mdn_loss(pi, sigma, mu, data):
+    """Calculates the error, given the MoG parameters and the target
+
+    The loss is the negative log likelihood of the data given the MoG
+    parameters.
+    """
+    prob = pi * gaussian_probability(sigma, mu, target)
+    nll = -torch.log(torch.sum(prob, dim=1))
+    return torch.mean(nll)
+
+
+def sample(pi, sigma, mu):
+    """Draw samples from a MoG.
+    """
+    categorical = Categorical(pi)
+    pis = list(categorical.sample().data)
+    sample = Variable(sigma.data.new(sigma.size(0), sigma.size(2)).normal_())
+    for i, idx in enumerate(pis):
+        sample[i] = sample[i].mul(sigma[i,idx]).add(mu[i,idx])
+    return sample
