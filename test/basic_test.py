b88bf3affe3c0b22cdeeb81451aa35d66786539e
Benjamin Bastian <benjamin.bastian@gmail.com>
2021-04-02 00:44:38 -0700

    Add basic test for the shape of the output of the mdn

---
+++ test/basic_test.py
@@ -0,0 +1,14 @@
+import unittest
+import torch
+from mdn import mdn
+
+class BasicMDN(unittest.TestCase):
+    def setUp(self):
+        self.mdn = mdn.MDN(4, 6, 10)
+
+    def test_output_shape(self):
+        minibatch = torch.randn((2, 4))
+        pi, sigma, mu = self.mdn(minibatch)
+        self.assertEqual(pi.size(), (2, 10))
+        self.assertEqual(sigma.size(), (2, 10, 6))
+        self.assertEqual(mu.size(), (2, 10, 6))
