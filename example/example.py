816f406189d9f4b667241690eba12d17db254839
Benjamin Bastian <benjamin.bastian@gmail.com>
2021-04-02 21:33:54 -0700

    Run autopep8 on example.py

--- example/example.py
+++ example/example.py
@@ -1,28 +1,30 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
+import matplotlib.pyplot as plt
 import sys
 sys.path.append('../mdn')
 import mdn
-import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 input_dims = 2
 output_dims = 1
 num_gaussians = 5
 
+
 def translate_cluster(cluster, dim, amount):
     """Translates a cluster in a particular dimension by some amount
     """
-    translation = torch.ones(cluster.size(0))*amount
+    translation = torch.ones(cluster.size(0)) * amount
     cluster.transpose(0, 1)[dim].add_(translation)
     return cluster
 
+
 print("Generating training data... ", end='')
-cluster1 = torch.randn((50, input_dims+output_dims))/4
+cluster1 = torch.randn((50, input_dims + output_dims)) / 4
 cluster1 = translate_cluster(cluster1, 1, 1.2)
-cluster2 = torch.randn((50, input_dims+output_dims))/4
+cluster2 = torch.randn((50, input_dims + output_dims)) / 4
 cluster2 = translate_cluster(cluster2, 0, -1.2)
-cluster3 = torch.randn((50, input_dims+output_dims))/4
+cluster3 = torch.randn((50, input_dims + output_dims)) / 4
 cluster3 = translate_cluster(cluster3, 2, -1.2)
 training_set = torch.cat([cluster1, cluster2, cluster3])
 print('Done')
@@ -41,8 +43,8 @@ print('Training model... ', end='')
 sys.stdout.flush()
 for epoch in range(1000):
     model.zero_grad()
-    pi, sigma, mu = model(training_set[:,0:input_dims])
-    loss = mdn.mdn_loss(pi, sigma, mu, training_set[:,input_dims:])
+    pi, sigma, mu = model(training_set[:, 0:input_dims])
+    loss = mdn.mdn_loss(pi, sigma, mu, training_set[:, input_dims:])
     loss.backward()
     optimizer.step()
     if epoch % 100 == 99:
@@ -51,7 +53,7 @@ for epoch in range(1000):
 print(' Done')
 
 print('Generating samples... ', end='')
-pi, sigma, mu = model(training_set[:,0:input_dims])
+pi, sigma, mu = model(training_set[:, 0:input_dims])
 samples = mdn.sample(pi, sigma, mu)
 print('Done')
 
@@ -59,9 +61,9 @@ print('Saving samples.png... ', end='')
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-xs = training_set[:,0]
-ys = training_set[:,1]
-zs = training_set[:,2]
+xs = training_set[:, 0]
+ys = training_set[:, 1]
+zs = training_set[:, 2]
 
 ax.scatter(xs, ys, zs, label='target')
 ax.scatter(xs, ys, samples, label='samples')
